cba4666dc54b4232b6ff3417ff34e2d06e5ee052
elinalak <lakida_v@icloud.com>
2022-11-16 16:30:24 +0100

    Step 4  Implemented a search field

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -2,10 +2,14 @@ import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "0401234567" },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNumber] = useState("");
+  const [filtered, setFiltered] = useState("");
 
   const addName = (event) => {
     event.preventDefault();
@@ -20,6 +24,7 @@ const App = () => {
     setPersons(persons.concat(personObject));
     setNumber("");
     setNewName("");
+
     // eslint-disable-next-line
     persons.map(function (person, index) {
       if (person.name === newName) {
@@ -39,9 +44,36 @@ const App = () => {
     setNumber(event.target.value);
   };
 
+  const handleFilterChange = (event) => {
+    setFiltered(event.target.value, `target filter`);
+  };
+
+  const filterPerson = (event) => {
+    console.log(filtered, `filtered`);
+    event.preventDefault();
+
+    let result = "no";
+    persons.map(function (person, index) {
+      if (filtered === person.name) {
+        setFiltered(`${person.name} ${person.number}`);
+        result = person.name;
+
+        return <li key={index}>{person.number}</li>;
+      }
+    });
+
+    return <p> is {result}</p>;
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <form onSubmit={filterPerson}>
+        filter shown with <input type="search" onChange={handleFilterChange} />{" "}
+        press enter
+      </form>
+      <p> {filtered}</p>
+      <h2>add a new</h2>
       <form onSubmit={addName}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
